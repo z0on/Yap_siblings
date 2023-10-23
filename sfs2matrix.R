1af029874b47e59b166c7191d47bca2648a34303
Mikhail V Matz <matz@utexas.edu>
2023-10-23 13:15:16 -0500

    Update sfs2matrix.R

--- sfs2matrix.R
+++ sfs2matrix.R
@@ -1,7 +1,7 @@
-
-sfs2matrix=function(sfs,n1,n2) {
+# converts one-liner 2dSFS to matrix that can be plotted
+sfs2matrix=function(sfs,n1,n2,zero.ends=TRUE) {
   dd=matrix(ncol=2*n1+1,nrow=2*n2+1,sfs)
-#  dd[1,1]=dd[2*n2+1,2*n1+1]=0
+ if(zero.ends=TRUE) { dd[1,1]=dd[2*n2+1,2*n1+1]=0 }
   return(apply(dd,2,rev))
 }
 
